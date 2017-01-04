942601bd6319e46d521f8237ff8a443de2afdf8d
Jeremy Clark <jeremy@jeremybytes.com>
2017-01-04 10:59:58 -0800

    Added files

---
+++ TestingFoq/Library1.fs
@@ -0,0 +1,22 @@
+﻿
+
+module Foq.Usage
+open NUnit.Framework
+
+open Foq
+
+let [<Test>] ``setup a method to always return true`` () =
+    // Arrange
+    let instance =
+        Mock<System.Collections.IList>()
+            .Setup(fun x -> <@ x.Contains(any()) @>).Returns(true)
+            .Create()
+    // Assert
+    Assert.IsTrue(instance.Contains("Anything"))
+
+
+let [<Test>] ``check test runner functionality`` () =
+    let add x y = x + y
+    let actual = add 2 3
+    Assert.AreEqual(5, actual)
+
---
+++ TestingFoq/Script.fsx
@@ -0,0 +1,19 @@
+﻿
+#r @"..\packages\Foq.1.7.1\Lib\net45\Foq.dll"
+#r @"..\packages\NUnit.3.5.0\lib\net45\nunit.framework.dll"
+
+open NUnit.Framework
+open Foq
+
+let [<Test>] ``setup a method to always return true`` () =
+    // Arrange
+    let instance =
+        Mock<System.Collections.IList>()
+            .Setup(fun x -> <@ x.Contains(any()) @>).Returns(true)
+            .Create()
+    // Assert
+    Assert.IsTrue(instance.Contains("Anything"))
+
+``setup a method to always return true``()
+
+
